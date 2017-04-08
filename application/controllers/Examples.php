a4327e13b47c5c85c5489dcc9b7f974efb38603e
iDeveloper2017 <bahrom1982@gmail.com>
2017-04-08 15:23:16 +0500

    CodeIgniter 3 Files

---
+++ application/controllers/Examples.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 08.04.2017
+ * Time: 15:21
+ */
+class Examples extends CI_Controller
+{
+
+    public function __construct(){
+        parent::__constuct();
+    }
+
+    public function index(){
+        echo "Hello Wolrd";
+    }
+}
