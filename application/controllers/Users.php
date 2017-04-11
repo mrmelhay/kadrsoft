b43c89828e00b78df6956d4e2b058e833156fb3f
iDeveloper2017 <bahrom1982@gmail.com>
2017-04-11 12:53:09 +0500

    CodeIgniter 3 Files

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -14,7 +14,6 @@ class Users extends MY_Controller{
 
     }
     public function index(){
-
         $this->data['title']='Òèçèìãà êèğèø';
         $this->data['content']=$this->load->view('/user/login',$this->data,true);
         $this->view_lib->layout($this->data);
