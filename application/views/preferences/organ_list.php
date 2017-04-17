e3a091200677c716623b5fef6b8eaeaba837615d
iDeveloper2017 <bahrom1982@gmail.com>
2017-04-17 13:38:57 +0500

    CodeIgniter 3 Files

--- application/views/preferences/organ_list.php
+++ application/views/preferences/organ_list.php
@@ -10,7 +10,7 @@
         <div class="adv-table editable-table ">
             <div class="clearfix">
                 <div class="btn-group">
-                    <button id="editable-sample_new" class="btn btn-primary">
+                    <button id="new_organ" type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
                         Янги қўшиш <i class="fa fa-plus"></i>
                     </button>
                 </div>
@@ -72,3 +72,37 @@
     </div><!--/porlets-content-->
 </div>
 </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                ....
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+//    $(document).ready(function() {
+//        $(document).on('click', "#editable-sample_new1", function () {
+//
+//        });
+//    });
+
+    $('#myModal').on('shown.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var recipient = "OK";
+        var modal = $(this);
+        modal.find('.modal-title').text('New message to ' + recipient);
+        modal.find('.modal-body').text(recipient);
+
+    })
+</script>
+
