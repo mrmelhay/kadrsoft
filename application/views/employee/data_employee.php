29fe210c9bbc096c5c50457db94567a1a1c111d0
Hayrulla <mrmelhay@gmail.com>
2017-05-02 15:06:00 +0500

    assets

--- application/views/employee/data_employee.php
+++ application/views/employee/data_employee.php
@@ -55,7 +55,7 @@
             </ul>
             <div class="tab-content" id="myTabContent">
                 <div id="Tab1" class="tab-pane fade in active">
-                    <?php include (base_url('employee/data/emp_passport'));?>
+                    <?php $this->load->view('employee/data/emp_passport');?>
                 </div>
                 <div id="Tab2" class="tab-pane fade">
                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet</p>
