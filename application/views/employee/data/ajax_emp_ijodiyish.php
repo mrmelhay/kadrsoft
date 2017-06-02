b747f7f4ac3a29ec48b4643306a0c497c854cde1
Hayrulla <mrmelhay@gmail.com>
2017-06-02 17:07:36 +0500

    Objective

---
+++ application/views/employee/data/ajax_emp_ijodiyish.php
@@ -0,0 +1,30 @@
+<div class="row">
+    <div class="col-md-10">
+        <div class="block-web">
+            <div class="porlets-content">
+
+
+                <div class="form-group">
+                    <label class="col-sm-5 control-label">Яратилган ижодий махсулот</label>
+                    <div class="col-sm-7">
+                        <input type="text" name="ijodiyish_name" class="form=control" id="ijodiyish_name" value="<?php echo $moderator['ijodiyish_name'];?>"/>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-5 control-label">Яратилган йили</label>
+                    <div class="col-sm-7">
+                        <input type="text" name="ijodiyish_year" class="form=control" id="ijodiyish_year" value="<?php echo $moderator['ijodiyish_year'];?>"/>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
