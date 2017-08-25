ad371d475593bb63acdc40d69f53891b045a9fce
Hayrulla <mrmelhay@gmail.com>
2017-08-25 17:10:09 +0500

    Help

---
+++ application/views/book/01.php
@@ -0,0 +1,10 @@
+<div class="container clear_both padding_fix">
+   <h2>Дастурга кириш</h2>
+   <h4><p style="padding-left: 50px; padding-top: 50px"> Дастурга кириш учун <a href="#">www.kadrsoft.nisd.uz</a> манзилига ихтиёрий веб браузер (Google Chrome браузери тавсия қилинади) орқали кириш ва тегишли логин ва паролни териш керак бўлади.</p></h4>
+<p align="center">
+  <img src="<?php echo base_url('images/bookimg/kadr_photo_001.png')?>" width="95%" height="95%" alt=""/>
+</p>
+
+   <h4 style="padding-left: 50px; padding-top: 50px">Логин ва паролни тергандан сўнг "Кириш" тугмасини босинг шундан сўнг дастур асосий ойнаси очилади </h4>
+   <p  align="center"> <img src="<?php echo base_url('images/bookimg/kadr_photo_002.png')  ?>" width="95%" alt=""/></p>
+</div>
