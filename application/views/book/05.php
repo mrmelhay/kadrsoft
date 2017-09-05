ec7fe1205f98c8cda4c1ee9fb5f8217353b5b052
Hayrulla <mrmelhay@gmail.com>
2017-09-05 12:29:56 +0500

    Help

--- application/views/book/05.php
+++ application/views/book/05.php
@@ -1,10 +1,10 @@
 <div class="container clear_both padding_fix">
-    <h2>Муассаса ҳақидаги маълумотларни киритиш</h2>
+    <h2>Янги ҳодим қўшиш</h2>
     <p>Муассаса ҳақидаги умумий маълумотлар қўйидаги суратда кўрсатиб ўтилган</p>
     <p>Муассасангиз ҳақидаги маълумотларни таҳрирлаш учун ойнанинг ўнг томонида жойлашган <button type="button" class="btn btn-primary btn-xs">Ўзгартириш</button> тугмасини босинг</p>
     <p align="center"><img src="<?php echo base_url('images/bookimg/kadr_photo_005.png')?>" width="95%" height="95%" alt="5-расм"/></p>
 
-   <hr>
+    <hr>
     <h2>Муассаса ҳақидаги маълумотларни ўзгартириш ойнаси</h2>
     <p align="center"><img src="<?php echo base_url('images/bookimg/kadr_photo_006.png')?>" width="95%" height="95%" alt="6-расм"/></p>
 
