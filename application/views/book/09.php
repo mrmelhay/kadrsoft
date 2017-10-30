8b279c422ec6cc00b16e94f2706f65c1432cdb65
Hayrulla <mrmelhay@gmail.com>
2017-10-30 12:08:59 +0500

    Help

--- application/views/book/09.php
+++ application/views/book/09.php
@@ -1,15 +1,21 @@
 <div class="container clear_both padding_fix">
     <h2>Илмий унвон ҳақидаги маълумотларни киритиш</h2>
     <p>Илмий унвон ҳақидаги маълумотларни киритиш учун ходим ҳақидаги қўшимча маълумотларни киритиш ойнасидан <strong>"Унвон"</strong> бўлимига ўтамиз.</p>
-    <p align="center"><img src="<?php echo base_url('images/bookimg/kadr_photo_011.png')?>" width="95%" height="95%" alt="11-расм"/></p>
-    <p>Янги тил ҳақидаги маълумотларини киритиш учун <button type="button" class="btn btn-success btn-xs">
+    <p align="center"><img src="<?php echo base_url('images/bookimg/kadr_photo_015.png')?>" width="95%" height="95%" alt="15-расм"/></p>
+    <p>Янги илмий унвон ҳақидаги маълумотларини киритиш учун <button type="button" class="btn btn-success btn-xs">
             <i class="fa fa-plus"></i> Янги қўшиш</button> тугмасини босинг.</p>
-    <p>Агарда сиз мавжуд тиллар ҳақидаги маълумотларини ўзгартирмоқчи бўлсангиз унда ойнада мавжуд тил номи устига босинг.</p>
+    <p>Агарда сиз мавжуд илмий унвон ҳақидаги маълумотларини ўзгартирмоқчи бўлсангиз унда ойнада мавжуд илмий унвон номи устига босинг.</p>
 
-    <p align="center"><img src="<?php echo base_url('images/bookimg/kadr_photo_012.png')?>" width="95%" height="95%" alt="12-расм"/></p>
+    <p align="center"><img src="<?php echo base_url('images/bookimg/kadr_photo_016.png')?>" width="95%" height="95%" alt="16-расм"/></p>
     <hr>
-    <p>Тилларни билиш даражаси ҳақидаги маълумотларни киритиб бўлгач</p>
+    <p>Илмий унвон ҳақидаги маълумотларни киритиб бўлгач</p>
     <ul>
+
+        <li><i class="fa fa-warning squre semiround icon_size3 "></i> Диққат!!! Агарда ҳодимда бир неча илмий унвон мавжуд бўлса, </br>
+            таълим бераётган соҳасига оид илмий унвондаги "Статус" <input type="checkbox"  readonly>
+            катакчасидаги белгини олиб қўйинг. </br>
+            Шундан сўнг дастур унинг жорий илмий унвони ҳақидаги маълумотларини кўрсатишни бошлайди</li>
+        <li><strong>Диплом нусхаси</strong> қисмига диплом нусҳасини юкланг. Файл формати <strong>JPG</strong>, хажми <strong> 1Мб</strong></li>
         <li>Ойнада қўрсатилган барча майдонларни тўлдиринг ва <button type="button" class="btn btn-primary btn-xs">Сақлаш</button> тугмасини босинг</li>
         <li>Агар маълумотларни ўзгартиришни ҳоҳламасангиз <button type="button" class="btn btn-default btn-xs">Чиқиш</button> тугмасини босинг</li>
 
