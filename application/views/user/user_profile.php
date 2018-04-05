8c9db35b1893caa4f67b8b6d83b18d75e51d6217
mrmelhay@gmail.com <pass2word>
2018-04-05 16:27:31 +0500

    Error Correction

--- application/views/user/user_profile.php
+++ application/views/user/user_profile.php
@@ -3,13 +3,13 @@
 <div class="container clear_both padding_fix">
     <div class="block-web">
         <div class="header">
-            <h3 class="content-header">Фойдаланувчи парольини алмаштириш</h3>
+            <h3 class="content-header">Фойдаланувчи паролини алмаштириш</h3>
         </div>
         <div class="porlets-content">
             <?php if ($this->session->flashdata('message') != null) { ?>
                 <div class="alert alert-info alert-styled-left alert-bordered">
                     <button type="button" class="close" data-dismiss="alert"><span>×</span><span
-                                class="sr-only">Закрыть</span></button>
+                                class="sr-only">Ёпиш</span></button>
                     <span class="text-semibold"><?php echo $this->session->flashdata('message'); ?></span>
                 </div>
             <?php } ?>
@@ -32,23 +32,23 @@
                       action="<?php echo base_url('users/update_login') ?>">
                     <input type="hidden" name="userid" value="<?php echo $userid; ?>"/>
                     <div class="form-group lable-padd">
-                        <label class="col-sm-2">Янги пароль:</label>
+                        <label class="col-sm-2">Янги парол:</label>
                         <div class="col-sm-6">
-                            <input type="password" placeholder="Янги пароль:" id="password" name="password"
+                            <input type="password" placeholder="Янги паролни китиринг:" id="password" name="password"
                                    class="form-control"/>
                         </div>
                     </div>
                     <div class="form-group lable-padd">
-                        <label class="col-sm-2 label-control">Такрорлаш пароль:</label>
+                        <label class="col-sm-2 label-control">Паролни такрорлаш:</label>
                         <div class="col-sm-6">
-                            <input type="password" placeholder="Такрорлаш пароль:" id="possword1" name="password1"
+                            <input type="password" placeholder="Паролни такрорланг:" id="possword1" name="password1"
                                    class="form-control"/>
                         </div>
                     </div>
                     <div class="form-group lable-padd">
                         <div class=" col-sm-10">
                             <div class="checkbox checkbox_margin">
-                                <button class="btn btn-info" type="submit">Саклаш</button>
+                                <button class="btn btn-info" type="submit">Сақлаш</button>
                             </div>
                         </div>
                     </div>
